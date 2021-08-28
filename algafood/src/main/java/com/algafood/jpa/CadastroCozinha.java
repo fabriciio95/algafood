c18bffd2625926fe4ac72e27d5364cdabbdab839
fabriciio95 <fabriciusiqueira@gmail.com>
2021-08-28 20:40:18 -0300

    Buscando um objeto pelo id no banco de dados

--- algafood/src/main/java/com/algafood/jpa/CadastroCozinha.java
+++ algafood/src/main/java/com/algafood/jpa/CadastroCozinha.java
@@ -21,6 +21,10 @@ public class CadastroCozinha {
 		return query.getResultList();
 	}
 	
+	public Cozinha buscar(Long id) {
+		return entityManager.find(Cozinha.class, id);
+	}
+	
 	@Transactional
 	public Cozinha adicionar(Cozinha cozinha) {
 		 return entityManager.merge(cozinha);
