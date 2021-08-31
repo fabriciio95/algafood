d21efe659b87019f2bc281434ae46aabc5151d2f
fabriciio95 <fabriciusiqueira@gmail.com>
2021-08-30 21:27:48 -0300

    Conhecendo e usando o Lombok

--- algafood/src/main/java/com/algafood/domain/model/Restaurante.java
+++ algafood/src/main/java/com/algafood/domain/model/Restaurante.java
@@ -8,12 +8,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import com.algafood.jpa.Cozinha;
 
-@Getter
-@Setter
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Restaurante {
--- algafood/src/main/java/com/algafood/jpa/Cozinha.java
+++ algafood/src/main/java/com/algafood/jpa/Cozinha.java
@@ -5,16 +5,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
 
-@Getter
-@Setter
+@Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
-@ToString
 public class Cozinha {
 
 	@EqualsAndHashCode.Include
