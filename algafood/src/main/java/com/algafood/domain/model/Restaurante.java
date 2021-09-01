cc498857ee101f74840bbdd3da21b66cd4fcb49a
fabriciio95 <fabriciusiqueira@gmail.com>
2021-08-31 21:36:04 -0300

    Desafio: Lombok e repositório de restaurantes

--- algafood/src/main/java/com/algafood/domain/model/Restaurante.java
+++ algafood/src/main/java/com/algafood/domain/model/Restaurante.java
@@ -8,8 +8,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import com.algafood.jpa.Cozinha;
-
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
--- algafood/src/main/resources/import.sql
+++ algafood/src/main/resources/import.sql
@@ -1,2 +1,5 @@
 INSERT INTO cozinha (nome) VALUES ('Tailandesa');
 INSERT INTO cozinha (nome) VALUES ('Indiana');
+
+INSERT INTO restaurante (nome, taxa_frete) VALUES ('Belo Sabor', 5);
+INSERT INTO restaurante (nome, taxa_frete) VALUES ('Bom Apetite', 7);
