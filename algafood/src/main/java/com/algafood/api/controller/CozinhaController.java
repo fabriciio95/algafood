780fa79d1e41e2d1866a9ab13534323b28efb834
fabriciio95 <fabriciusiqueira@gmail.com>
2021-09-09 19:57:54 -0300

    Modelando e implementando a inclusão de recursos com POST

--- algafood/src/main/java/com/algafood/api/controller/CozinhaController.java
+++ algafood/src/main/java/com/algafood/api/controller/CozinhaController.java
@@ -9,7 +9,10 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algafood.api.model.CozinhasXMLWrapper;
@@ -33,6 +36,12 @@ public class CozinhaController {
 	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
 	public CozinhasXMLWrapper listarXml() {
 		return new CozinhasXMLWrapper(cozinhaRepository.listar());
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cozinha adicionar(@RequestBody Cozinha cozinha) {
+		return cozinhaRepository.salvar(cozinha);
 	}
 	
 //	//@ResponseStatus(HttpStatus.CREATED)
