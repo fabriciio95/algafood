6a8c3465a63b3783d09d618e175fca176f1186e5
fabriciio95 <fabriciusiqueira@gmail.com>
2021-10-12 17:49:29 -0300

    Refatorando o código do projeto para usar o repositório do SDJ

--- algafood/src/main/java/com/algafood/api/controller/TesteController.java
+++ algafood/src/main/java/com/algafood/api/controller/TesteController.java
@@ -1,13 +1,8 @@
 package com.algafood.api.controller;
 
-import java.util.List;
-
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.algafood.domain.model.Cozinha;
 import com.algafood.domain.repository.CozinhaRepository;
 
 import lombok.AllArgsConstructor;
@@ -19,9 +14,9 @@ public class TesteController {
 	
 	private CozinhaRepository cozinhaRepository;
 
-	@GetMapping("/cozinhas/por-nome")
-	public List<Cozinha> cozinhasPorNome(@RequestParam("nome") String nome) {
-		return cozinhaRepository.consultarPorNome(nome);
-	}
+//	@GetMapping("/cozinhas/por-nome")
+//	public List<Cozinha> cozinhasPorNome(@RequestParam("nome") String nome) {
+//		return cozinhaRepository.consultarPorNome(nome);
+//	}
 
 }
