f81b7c81b832fa978169550915435ffe537140d9
fabriciio95 <fabriciusiqueira@gmail.com>
2021-10-26 20:11:00 -0300

    :hammer: retirando comentários das classes

--- algafood/src/main/java/com/algafood/domain/repository/RestauranteRepository.java
+++ algafood/src/main/java/com/algafood/domain/repository/RestauranteRepository.java
@@ -11,20 +11,16 @@ import org.springframework.stereotype.Repository;
 import com.algafood.domain.model.Restaurante;
 
 @Repository
-public interface RestauranteRepository 
-					extends CustomJpaRepository<Restaurante, Long>, RestauranteRepositoryQueries, JpaSpecificationExecutor<Restaurante> {
+public interface RestauranteRepository extends CustomJpaRepository<Restaurante, Long>, RestauranteRepositoryQueries,
+		JpaSpecificationExecutor<Restaurante> {
 
-	/*Prefixos para buscar: read, get, query, stream e find*/
 	List<Restaurante> queryByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
-	
-	//@Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+
 	List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinhaId);
-	
-	//List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinhaId);
-	
+
 	Optional<Restaurante> getFirstByNomeContaining(String nome);
-	
+
 	List<Restaurante> readTop2ByNomeContaining(String nome);
-	
+
 	int countByCozinhaId(Long cozinhaId);
 }
