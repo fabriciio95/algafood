a7330c214d12e0fc088dacee072bf369870fc0c7
fabriciio95 <fabriciusiqueira@gmail.com>
2021-11-21 21:17:21 -0300

    Tratando exceções com nível de controlador com @ExceptionHandler

--- algafood/src/main/java/com/algafood/api/controller/CidadeController.java
+++ algafood/src/main/java/com/algafood/api/controller/CidadeController.java
@@ -1,10 +1,13 @@
 package com.algafood.api.controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +17,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algafood.api.exceptionhandler.Problema;
+import com.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algafood.domain.exception.EstadoNaoEncontradoException;
 import com.algafood.domain.exception.NegocioException;
 import com.algafood.domain.model.Cidade;
@@ -64,6 +69,7 @@ public class CidadeController {
 			return cadastroCidade.salvar(cidadeAtual);
 			
 		} catch (EstadoNaoEncontradoException e) {
+			//throw new NegocioException(e.getMessage());
 			throw new NegocioException(e.getMessage(), e);
 		}
 	}
@@ -72,5 +78,27 @@ public class CidadeController {
 	@DeleteMapping("/{id}")
 	public void excluir(@PathVariable Long id) {
 		cadastroCidade.excluir(id);
+	}
+	
+	@ExceptionHandler(EntidadeNaoEncontradaException.class)
+	public ResponseEntity<?> tratarEntidadeNaoEncontradaException(
+			EntidadeNaoEncontradaException e) {
+		Problema problema = Problema.builder()
+				.dataHora(LocalDateTime.now())
+				.mensagem(e.getMessage()).build();
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND)
+				.body(problema);
+	}
+	
+	@ExceptionHandler(NegocioException.class)
+	public ResponseEntity<?> tratarNegocioException(
+			NegocioException e) {
+		Problema problema = Problema.builder()
+				.dataHora(LocalDateTime.now())
+				.mensagem(e.getMessage()).build();
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+				.body(problema);
 	}
 }
