c0a1c7a4745cc0c7a619c91ab0b937928acda405
fabriciio95 <fabriciusiqueira@gmail.com>
2021-11-22 21:38:44 -0300

    Desafio: implementando exception handler

--- algafood/src/main/java/com/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ algafood/src/main/java/com/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.algafood.domain.exception.EntidadeEmUsoException;
 import com.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algafood.domain.exception.NegocioException;
 
@@ -23,6 +24,15 @@ public class ApiExceptionHandler {
 		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND)
 				.body(problema);
+	}
+	
+	@ExceptionHandler(EntidadeEmUsoException.class)
+	public ResponseEntity<?> tratarEntidadeEmUsoException(EntidadeEmUsoException e) {
+		Problema problema = Problema.builder()
+				.dataHora(LocalDateTime.now())
+				.mensagem(e.getMessage()).build();
+		
+		return ResponseEntity.status(HttpStatus.CONFLICT).body(problema);
 	}
 	
 	@ExceptionHandler(NegocioException.class)
