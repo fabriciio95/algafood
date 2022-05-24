8edb82a41620fba538d1fde73e763f3cd8253bb6
Fabricio Macedo <fabriciusiqueira@gmail.com>
2022-05-24 20:22:46 -0300

    Limpando e populando o banco de dados de teste

--- algafood/src/test/java/com/algafood/CadastroCozinhaApiIT.java
+++ algafood/src/test/java/com/algafood/CadastroCozinhaApiIT.java
@@ -1,9 +1,9 @@
 package com.algafood;
 
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.hasSize;
 
-import org.flywaydb.core.Flyway;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,7 +14,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static io.restassured.RestAssured.given;
+import com.algafood.domain.model.Cozinha;
+import com.algafood.domain.repository.CozinhaRepository;
+import com.algafood.util.DatabaseCleaner;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
@@ -28,7 +30,10 @@ public class CadastroCozinhaApiIT {
 	private int port;
 	
 	@Autowired
-	private Flyway flyway;
+	private DatabaseCleaner databaseCleaner;
+	
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
 	
 	@Before
 	public void setup() {
@@ -36,7 +41,8 @@ public class CadastroCozinhaApiIT {
 		RestAssured.port = port;
 		RestAssured.basePath = "/cozinhas";
 		
-		flyway.migrate();
+		databaseCleaner.clearTables();
+		prepararDados();
 	}
 
 	@Test
@@ -51,15 +57,15 @@ public class CadastroCozinhaApiIT {
 	}
 	
 	@Test
-	public void deveriaConter4Cozinhas_QuandoConsultarCozinhas() {
+	public void deveriaConter2Cozinhas_QuandoConsultarCozinhas() {
 		
 		given()
 			.accept(ContentType.JSON)
 	   .when()
 	   		.get()
 	   .then()
-	   		.body("", hasSize(4))
-	   		.body("nome", hasItems("Indiana", "Tailandesa"));
+	   		.body("", hasSize(2))
+	   		.body("nome", hasItems("Americana", "Tailandesa"));
 	}
 	
 	@Test
@@ -73,5 +79,15 @@ public class CadastroCozinhaApiIT {
 			.post()
 		.then()
 			.statusCode(HttpStatus.CREATED.value());
+	}
+	
+	private void prepararDados() {
+		Cozinha cozinha1 = new Cozinha();
+		cozinha1.setNome("Tailandesa");
+		cozinhaRepository.save(cozinha1);
+		
+		Cozinha cozinha2 = new Cozinha();
+		cozinha2.setNome("Americana");
+		cozinhaRepository.save(cozinha2);
 	}
 }
