cecf0e2c49dc11d2087c8f4e31eead943bb29fa0
Fabricio Macedo <fabriciusiqueira@gmail.com>
2022-07-19 20:25:54 -0300

    Implementando paginação e ordenação em recursos de coleção da API

--- algafood/src/main/java/com/algafood/api/controller/CozinhaController.java
+++ algafood/src/main/java/com/algafood/api/controller/CozinhaController.java
@@ -5,6 +5,10 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -41,8 +45,14 @@ public class CozinhaController {
 	private CozinhaDTODisassembler cozinhaDTODisassembler;
 
 	@GetMapping
-	public List<CozinhaDTO> listar() {
-		return cozinhaDTOAssembler.toListDTO(cozinhaRepository.findAll());
+	public Page<CozinhaDTO> listar(@PageableDefault(size = 10) Pageable pageable) {
+		Page<Cozinha> cozinhasPage = cozinhaRepository.findAll(pageable);
+		
+		List<CozinhaDTO> cozinhasDTO = cozinhaDTOAssembler.toListDTO(cozinhasPage.getContent());
+		
+		Page<CozinhaDTO> cozinhaPageDTO = new PageImpl<>(cozinhasDTO, pageable, cozinhasPage.getTotalElements());
+		
+		return cozinhaPageDTO;
 	}
 
 	@GetMapping("/{cozinhaId}")
