cff74f85de41c2c2f3bcae4b3d941158ad7d1772
Fabricio Macedo <fabriciusiqueira@gmail.com>
2022-07-21 20:28:16 -0300

    Desafio: adicionando os filtros na consulta de vendas diárias

--- algafood/src/main/java/com/algafood/infrastructure/service/VendaQueryServiceImpl.java
+++ algafood/src/main/java/com/algafood/infrastructure/service/VendaQueryServiceImpl.java
@@ -1,15 +1,18 @@
 package com.algafood.infrastructure.service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.Predicate;
 
 import org.springframework.stereotype.Repository;
 
 import com.algafood.domain.filter.VendaDiariaFilter;
 import com.algafood.domain.model.Pedido;
+import com.algafood.domain.model.StatusPedido;
 import com.algafood.domain.model.dto.VendaDiaria;
 import com.algafood.domain.service.VendaQueryService;
 
@@ -35,6 +38,25 @@ public class VendaQueryServiceImpl implements VendaQueryService {
 		query.select(selection);
 		
 		query.groupBy(functionDateDataCriacao);
+		
+		var predicates = new ArrayList<>();
+		
+		if(filtro.getDataCriacaoInicio() != null) {
+			predicates.add(builder.greaterThanOrEqualTo(root.get("dataCriacao"), filtro.getDataCriacaoInicio()));
+		}
+		
+		if(filtro.getDataCriacaoFim() != null) {
+			predicates.add(builder.lessThanOrEqualTo(root.get("dataEntrega"), filtro.getDataCriacaoFim()));
+		}
+		
+		if(filtro.getRestauranteId() != null) {
+			predicates.add(builder.equal(root.get("restaurante"), filtro.getRestauranteId()));
+		}
+		
+		predicates.add(root.get("status").in(StatusPedido.CONFIRMADO, StatusPedido.ENTREGUE));
+		
+		
+		query.where(builder.and(predicates.toArray(new Predicate[0])));
 		
 		return manager.createQuery(query).getResultList();
 	}
