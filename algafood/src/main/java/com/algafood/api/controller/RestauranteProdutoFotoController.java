bd242ede1a239f1a643d97bbf58d4b3d4eb5420a
Fabricio Macedo <fabriciusiqueira@gmail.com>
2022-07-27 21:48:20 -0300

    Validando o tamanho máximo do arquivo

--- algafood/src/main/java/com/algafood/api/controller/RestauranteProdutoFotoController.java
+++ algafood/src/main/java/com/algafood/api/controller/RestauranteProdutoFotoController.java
@@ -3,13 +3,15 @@ package com.algafood.api.controller;
 import java.nio.file.Path;
 import java.util.UUID;
 
+import javax.validation.Valid;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.algafood.api.model.input.FotoProdutoInput;
+import com.algafood.api.model.input.FotoProdutoInputDTO;
 
 @RestController
 @RequestMapping("/restaurantes/{restauranteId}/produtos/{produtoId}/foto")
@@ -17,7 +19,7 @@ public class RestauranteProdutoFotoController {
 
 	@PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 	public void atualizarFoto(@PathVariable Long restauranteId, 
-			@PathVariable Long produtoId, FotoProdutoInput fotoProdutoInput) {
+			@PathVariable Long produtoId, @Valid FotoProdutoInputDTO fotoProdutoInput) {
 		
 		var nomeArquivo = UUID.randomUUID().toString() + "_" + fotoProdutoInput.getArquivo().getOriginalFilename();
 		
--- algafood/src/main/java/com/algafood/api/model/input/FotoProdutoInputDTO.java
+++ algafood/src/main/java/com/algafood/api/model/input/FotoProdutoInputDTO.java
@@ -1,15 +1,23 @@
 package com.algafood.api.model.input;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 import org.springframework.web.multipart.MultipartFile;
+
+import com.algafood.core.validation.FileSize;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class FotoProdutoInput {
+public class FotoProdutoInputDTO {
 
+	@NotNull
+	@FileSize(max = "500KB")
 	private MultipartFile arquivo;
 	
+	@NotBlank
 	private String descricao;
 }
