0429dbacdad345271002f94c9753218b7cc28b41
Fabricio Macedo <fabriciusiqueira@gmail.com>
2022-08-08 21:06:50 -0300

    Integrando o serviço de catálogo de fotos com o serviço de armazenagem

--- algafood/src/main/java/com/algafood/api/controller/RestauranteProdutoFotoController.java
+++ algafood/src/main/java/com/algafood/api/controller/RestauranteProdutoFotoController.java
@@ -1,5 +1,7 @@
 package com.algafood.api.controller;
 
+import java.io.IOException;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,7 +35,7 @@ public class RestauranteProdutoFotoController {
 
 	@PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 	public FotoProdutoDTO atualizarFoto(@PathVariable Long restauranteId, 
-			@PathVariable Long produtoId, @Valid FotoProdutoInputDTO fotoProdutoInput) {
+			@PathVariable Long produtoId, @Valid FotoProdutoInputDTO fotoProdutoInput) throws IOException {
 		
 		Produto produto = cadastroProdutoService.buscarOuFalhar(restauranteId, produtoId);
 		
@@ -47,7 +49,7 @@ public class RestauranteProdutoFotoController {
 		foto.setNomeArquivo(arquivo.getOriginalFilename());
 		
 		
-		foto = catalogoFotoProdutoService.salvar(foto);
+		foto = catalogoFotoProdutoService.salvar(foto, arquivo.getInputStream());
 		
 		return fotoProdutoAssembler.toDTO(foto);
 		
