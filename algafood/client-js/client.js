7dacb7fdd5bd94304ec831384789c16b1230a49d
Fabricio Macedo <fabriciusiqueira@gmail.com>
2022-09-28 19:50:19 -0300

    Desafio: implementando uma requisição GET com JavaScript

--- algafood/client-js/client.js
+++ algafood/client-js/client.js
@@ -16,20 +16,34 @@ function consultarRestaurantes() {
 
 }
 
-function fecharRestaurante() {
-
-	$.ajax({
-	
-		url: "http://api.algafood.local:8080/restaurantes/1/fechamento",
-		type: "put",
-		
-		
-		success: function(response) {
-				alert("Restaurante foi fechado!");
-		}
-	
-	});
-
+function consultar() {
+   
+   $.ajax({
+   		url: "http://api.algafood.local:8080/formas-pagamento",
+   		type: "get",
+   		
+   		success: function(response) {
+   			preencherTabela(response);
+   		}
+   
+   });
 }
 
-$("#botao").click(consultarRestaurantes);
+
+function preencherTabela(formasPagamento) {
+  $("#tabela tbody tr").remove();
+
+  $.each(formasPagamento, function(i, formaPagamento) {
+    var linha = $("<tr>");
+
+    linha.append(
+      $("<td>").text(formaPagamento.id),
+      $("<td>").text(formaPagamento.descricao)
+    );
+
+    linha.appendTo("#tabela");
+  });
+}
+
+
+$("#btn-consultar").click(consultar);
