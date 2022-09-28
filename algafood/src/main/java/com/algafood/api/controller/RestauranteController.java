f039d4ce60378d9312b722ad013a9d1ea0b2d452
Fabricio Macedo <fabriciusiqueira@gmail.com>
2022-09-27 21:29:28 -0300

    Habilitando CORS globalmente no projeto da API

--- algafood/src/main/java/com/algafood/api/controller/RestauranteController.java
+++ algafood/src/main/java/com/algafood/api/controller/RestauranteController.java
@@ -16,7 +16,6 @@ import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.util.ReflectionUtils;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.SmartValidator;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -45,7 +44,7 @@ import com.fasterxml.jackson.annotation.JsonView;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-@CrossOrigin(maxAge = 10)//(origins = { "http://www.algafood.local:8000", "http://www.matafome.local:8000" })
+//@CrossOrigin(maxAge = 10)//(origins = { "http://www.algafood.local:8000", "http://www.matafome.local:8000" })
 @RestController
 @RequestMapping("/restaurantes")
 public class RestauranteController {
