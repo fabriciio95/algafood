7c5a26ba91e9d0d15300239f0210c235844a3b1e
Fabricio Macedo <fabriciusiqueira@gmail.com>
2022-10-12 20:59:49 -0300

    Selecionando os endpoints da API para gerar a documentação

--- algafood/src/main/java/com/algafood/core/openapi/SpringFoxConfig.java
+++ algafood/src/main/java/com/algafood/core/openapi/SpringFoxConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.google.common.base.Predicates;
+
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -18,8 +21,13 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 	public Docket apiDocket() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
-					.apis(RequestHandlerSelectors.any())
-					.build();
+//					.apis(Predicates.and(
+//							RequestHandlerSelectors.basePackage("com.algafood.api"),
+//							RequestHandlerSelectors.basePackage("com.algafood.outro")))
+//					.paths(PathSelectors.ant("/restaurantes/*"))
+					.apis(RequestHandlerSelectors.basePackage("com.algafood.api"))
+					.paths(PathSelectors.any())
+				.build();
 	}
 	
 	@Override
