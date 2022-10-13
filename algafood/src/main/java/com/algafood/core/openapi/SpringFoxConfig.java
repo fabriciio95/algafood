2491f807f45ef1d4c69398e656b56932998df2db
Fabricio Macedo <fabriciusiqueira@gmail.com>
2022-10-12 21:05:33 -0300

    Descrevendo informações da API na documentação

--- algafood/src/main/java/com/algafood/core/openapi/SpringFoxConfig.java
+++ algafood/src/main/java/com/algafood/core/openapi/SpringFoxConfig.java
@@ -7,8 +7,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.google.common.base.Predicates;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -27,6 +30,16 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 //					.paths(PathSelectors.ant("/restaurantes/*"))
 					.apis(RequestHandlerSelectors.basePackage("com.algafood.api"))
 					.paths(PathSelectors.any())
+				.build()
+				.apiInfo(apiInfo());
+	}
+	
+	public ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+					.title("Algafood API")
+					.description("API aberta para clientes e restaurantes")
+					.version("1")
+					.contact(new Contact("Fabricio", "https://github.com/fabriciio95", "fabriciousiqueira@gmail.com"))
 				.build();
 	}
 	
