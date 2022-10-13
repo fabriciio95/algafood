5169a92e531b8b74d583a5574ffd86d6b4b14c29
Fabricio Macedo <fabriciusiqueira@gmail.com>
2022-10-13 20:24:24 -0300

    Descrevendo modelos de representações e suas propriedades

--- algafood/src/main/java/com/algafood/api/model/CidadeDTO.java
+++ algafood/src/main/java/com/algafood/api/model/CidadeDTO.java
@@ -1,14 +1,19 @@
 package com.algafood.api.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+//@ApiModel(value = "Cidade", description = "Representa uma cidade")
 @Getter
 @Setter
 public class CidadeDTO {
 
+	//@ApiModelProperty(value = "ID da cidade", example = "1")
+	@ApiModelProperty(example = "1")
 	private Long id;
 	
+	@ApiModelProperty(example = "Uberlândia")
 	private String nome;
 	
 	private EstadoDTO estado;
