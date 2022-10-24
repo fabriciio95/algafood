b28c45825daac06fb6b0ee377f65f6654812b5eb
Fabricio Macedo <fabriciusiqueira@gmail.com>
2022-10-24 20:48:16 -0300

    Desafio: descrevendo documentação de endpoints de associação de restaurantes com usuários

---
+++ algafood/src/main/java/com/algafood/api/openapi/controller/RestauranteUsuarioControllerOpenApi.java
@@ -0,0 +1,39 @@
+package com.algafood.api.openapi.controller;
+
+import java.util.List;
+
+import com.algafood.api.exceptionhandler.Problem;
+import com.algafood.api.model.UsuarioDTO;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@Api(tags = "Restaurantes")
+public interface RestauranteUsuarioControllerOpenApi {
+
+	@ApiResponses({
+		@ApiResponse(code = 404, message = "Restaurante não encontrado", response = Problem.class)
+	})
+	@ApiOperation("Lista os usuários responsáveis associados ao restaurante")
+	List<UsuarioDTO> listar(@ApiParam(value = "ID do restaurante", example = "1", required = true) Long restauranteId);
+
+	@ApiResponses({
+		@ApiResponse(code = 404, message = "Restaurante ou usuário não encontrado", response = Problem.class),
+		@ApiResponse(code = 204, message = "Associação realizada com sucesso")
+	})
+	@ApiOperation("Associação de restaurante com usuário responsável")
+	void associarResponsavel(@ApiParam(value = "ID do restaurante", example = "1", required = true) Long restauranteId, 
+			@ApiParam(value = "ID do usuário", example = "1", required = true)  Long usuarioId);
+
+	@ApiResponses({
+		@ApiResponse(code = 404, message = "Restaurante ou usuário não encontrado", response = Problem.class),
+		@ApiResponse(code = 204, message = "Desassociação realizada com sucesso")
+	})
+	@ApiOperation("Desassociação de restaurante com usuário responsáve")
+	void deassociarResponsavel(@ApiParam(value = "ID do restaurante", example = "1", required = true) Long restauranteId, 
+			@ApiParam(value = "ID do usuário", example = "1", required = true)  Long usuarioId);
+
+}
