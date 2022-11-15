197e37dd142c3acdfdfc41fe51c18117abd81b7a
Fabricio Macedo <fabriciusiqueira@gmail.com>
2022-11-15 20:51:17 -0300

    Desafio: corrigindo a documentação dos endpoints de produtos

--- algafood/src/main/java/com/algafood/core/springfox/SpringFoxConfig.java
+++ algafood/src/main/java/com/algafood/core/springfox/SpringFoxConfig.java
@@ -31,6 +31,7 @@ import com.algafood.api.model.EstadoDTO;
 import com.algafood.api.model.GrupoDTO;
 import com.algafood.api.model.PedidoResumoDTO;
 import com.algafood.api.model.PermissaoDTO;
+import com.algafood.api.model.ProdutoDTO;
 import com.algafood.api.openapi.model.CidadesModelOpenApi;
 import com.algafood.api.openapi.model.CozinhasDTOOpenApi;
 import com.algafood.api.openapi.model.EnderecoInputModelOpenApi;
@@ -40,6 +41,7 @@ import com.algafood.api.openapi.model.LinksModelOpenApi;
 import com.algafood.api.openapi.model.PageableModelOpenApi;
 import com.algafood.api.openapi.model.PedidosResumoModelOpenApi;
 import com.algafood.api.openapi.model.PermissoesModelOpenApi;
+import com.algafood.api.openapi.model.ProdutosModelOpenApi;
 import com.algafood.api.openapi.model.RestauranteInputModelOpenApi;
 import com.fasterxml.classmate.TypeResolver;
 
@@ -99,7 +101,8 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 						AlternateTypeRules.newRule(typeResolver.resolve(CollectionModel.class, CidadeDTO.class), CidadesModelOpenApi.class),
 						AlternateTypeRules.newRule(typeResolver.resolve(CollectionModel.class, EstadoDTO.class), EstadosModelOpenApi.class),
 						AlternateTypeRules.newRule(typeResolver.resolve(CollectionModel.class, GrupoDTO.class), GruposModelOpenApi.class),
-						AlternateTypeRules.newRule(typeResolver.resolve(CollectionModel.class, PermissaoDTO.class), PermissoesModelOpenApi.class))
+						AlternateTypeRules.newRule(typeResolver.resolve(CollectionModel.class, PermissaoDTO.class), PermissoesModelOpenApi.class),
+						AlternateTypeRules.newRule(typeResolver.resolve(CollectionModel.class, ProdutoDTO.class), ProdutosModelOpenApi.class))
 				.apiInfo(apiInfo())
 				.tags(new Tag("Cidades", "Gerencia as cidades"),
 					  new Tag("Grupos", "Gerencia os grupos"),
