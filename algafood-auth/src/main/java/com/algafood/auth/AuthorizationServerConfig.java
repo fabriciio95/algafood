7c0b0c114a9eaf4c383bf830c89bb5635287269d
Fabricio Macedo <fabriciusiqueira@gmail.com>
2023-01-06 19:45:28 -0300

    Configurando a validade e não reutilização de Refresh Tokens

--- algafood-auth/src/main/java/com/algafood/auth/AuthorizationServerConfig.java
+++ algafood-auth/src/main/java/com/algafood/auth/AuthorizationServerConfig.java
@@ -31,7 +31,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 					.secret(passwordEncoder.encode("web123"))
 					.authorizedGrantTypes("password", "refresh_token")
 					.scopes("write", "read")
-					.accessTokenValiditySeconds(30) // 6 horas (padrão é 12 horas)
+					.accessTokenValiditySeconds(60 * 60 * 6) // 6 horas (padrão é 12 horas)
+					.refreshTokenValiditySeconds(60 * 24 * 60 * 60) // 60 dias 
 				.and()
 					.withClient("app-mobile")
 					.secret(passwordEncoder.encode("mob123"))
@@ -56,7 +57,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints
 				.authenticationManager(authenticationManager)
-				.userDetailsService(userDetailsService);
+				.userDetailsService(userDetailsService)
+				.reuseRefreshTokens(false);
 	}
 	
 }
