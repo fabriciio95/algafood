5b0dd945c8cfd7372e20cdf6db245980ba007082
Fabricio Macedo <fabriciusiqueira@gmail.com>
2023-01-25 20:09:05 -0300

    Revisando o fluxo de aprovação do Authorization Code com JWT

--- algafood-auth/src/main/java/com/algafood/auth/AuthorizationServerConfig.java
+++ algafood-auth/src/main/java/com/algafood/auth/AuthorizationServerConfig.java
@@ -17,6 +17,8 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.CompositeTokenGranter;
 import org.springframework.security.oauth2.provider.TokenGranter;
+import org.springframework.security.oauth2.provider.approval.ApprovalStore;
+import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
@@ -86,7 +88,16 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 				.reuseRefreshTokens(false)
 				//.tokenStore(redisTokenStore())
 				.tokenGranter(tokenGranter(endpoints))
-				.accessTokenConverter(jwtAccessTokenConverter());
+				.accessTokenConverter(jwtAccessTokenConverter())
+				.approvalStore(approvalStore(endpoints.getTokenStore()));
+	}
+	
+	private ApprovalStore approvalStore(TokenStore tokenStore) {
+		var approvalStore = new TokenApprovalStore();
+		
+		approvalStore.setTokenStore(tokenStore);
+		
+		return approvalStore;
 	}
 	
 	@SuppressWarnings("unused")
