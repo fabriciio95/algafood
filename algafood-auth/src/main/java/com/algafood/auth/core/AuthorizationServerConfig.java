0b8f344395466f45307f4e146e1be94da2c4716e
Fabricio Macedo <fabriciusiqueira@gmail.com>
2023-02-13 21:23:58 -0300

    Configurando os clientes OAuth2 em um banco de dados SQL

--- algafood-auth/src/main/java/com/algafood/auth/core/AuthorizationServerConfig.java
+++ algafood-auth/src/main/java/com/algafood/auth/core/AuthorizationServerConfig.java
@@ -2,6 +2,8 @@ package com.algafood.auth.core;
 
 import java.util.Arrays;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +11,6 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -29,8 +30,8 @@ import org.springframework.security.oauth2.provider.token.store.redis.RedisToken
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 	
-	@Autowired
-	private PasswordEncoder passwordEncoder;
+//	@Autowired
+//	private PasswordEncoder passwordEncoder;
 	
 	@Autowired
 	private AuthenticationManager authenticationManager;
@@ -41,36 +42,41 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Autowired
 	private JwtKeyStoreProperties jwtKeyStoreProperties;
 	
+	@Autowired
+	private DataSource dataSource;
+	
 	private RedisConnectionFactory redisConnectionFactory;
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		clients.inMemory()
-					.withClient("algafood-web")
-					.secret(passwordEncoder.encode("web123"))
-					.authorizedGrantTypes("password", "refresh_token")
-					.scopes("WRITE", "READ")
-					.accessTokenValiditySeconds(60 * 60 * 6) // 6 horas (padrão é 12 horas)
-					.refreshTokenValiditySeconds(60 * 24 * 60 * 60) // 60 dias 
-				.and()
-					.withClient("foodanalytics")
-					.secret(passwordEncoder.encode("food123"))
-					.authorizedGrantTypes("authorization_code")
-					.scopes("WRITE", "READ")
-					.redirectUris("http://localhost:8082")
-				.and()
-					.withClient("faturamento")
-					.secret(passwordEncoder.encode("fat123"))
-					.authorizedGrantTypes("client_credentials")
-				    .scopes("READ")
-				.and()
-					.withClient("webadmin")
-					.authorizedGrantTypes("implicit")
-					.scopes("READ", "WRITE")
-					.redirectUris("http://aplicacao-cliente")
-				.and()
-					.withClient("checktoken")
-					.secret(passwordEncoder.encode("check123"));
+//		clients.inMemory()
+//					.withClient("algafood-web")
+//					.secret(passwordEncoder.encode("web123"))
+//					.authorizedGrantTypes("password", "refresh_token")
+//					.scopes("WRITE", "READ")
+//					.accessTokenValiditySeconds(60 * 60 * 6) // 6 horas (padrão é 12 horas)
+//					.refreshTokenValiditySeconds(60 * 24 * 60 * 60) // 60 dias 
+//				.and()
+//					.withClient("foodanalytics")
+//					.secret(passwordEncoder.encode("food123"))
+//					.authorizedGrantTypes("authorization_code")
+//					.scopes("WRITE", "READ")
+//					.redirectUris("http://localhost:8082")
+//				.and()
+//					.withClient("faturamento")
+//					.secret(passwordEncoder.encode("fat123"))
+//					.authorizedGrantTypes("client_credentials")
+//				    .scopes("READ")
+//				.and()
+//					.withClient("webadmin")
+//					.authorizedGrantTypes("implicit")
+//					.scopes("READ", "WRITE")
+//					.redirectUris("http://aplicacao-cliente")
+//				.and()
+//					.withClient("checktoken")
+//					.secret(passwordEncoder.encode("check123"));
+		
+		clients.jdbc(dataSource);
 	}
 	
 	@Override
