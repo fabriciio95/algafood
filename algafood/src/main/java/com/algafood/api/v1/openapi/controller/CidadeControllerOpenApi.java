cfe06cdbf7dc733151290d3ff8fcbf9f181c8152
Fabricio Macedo <fabriciusiqueira@gmail.com>
2023-05-18 20:56:23 -0300

    Descrevendo tafs na documentação e associando com controllers

--- algafood/src/main/java/com/algafood/api/v1/openapi/controller/CidadeControllerOpenApi.java
+++ algafood/src/main/java/com/algafood/api/v1/openapi/controller/CidadeControllerOpenApi.java
@@ -6,8 +6,10 @@ import com.algafood.api.v1.model.CidadeDTO;
 import com.algafood.api.v1.model.input.CidadeInputDTO;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @SecurityRequirement(name = "security_auth")
+@Tag(name = "Cidades")
 public interface CidadeControllerOpenApi {
 
 	CollectionModel<CidadeDTO> listar();
