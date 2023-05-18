15de171d58ebcdb27844440f41775766b632811c
Fabricio Macedo <fabriciusiqueira@gmail.com>
2023-05-18 20:06:41 -0300

    Configurando múltiplas documentações em um só projeto

--- algafood/src/main/java/com/algafood/core/springdoc/SpringDocConfig.java
+++ algafood/src/main/java/com/algafood/core/springdoc/SpringDocConfig.java
@@ -1,5 +1,6 @@
 package com.algafood.core.springdoc;
 
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,7 +12,7 @@ import io.swagger.v3.oas.models.info.License;
 @Configuration
 public class SpringDocConfig {
 
-	@Bean
+	//@Bean
 	OpenAPI openAPI() {
 		return new OpenAPI()
 				.info(new Info()
@@ -26,5 +27,47 @@ public class SpringDocConfig {
 						 .description("AlgaWorks")
 						 .url("https://algaworks.com")
 				 );
+	}
+	
+	@Bean
+	GroupedOpenApi groupedOpenApi() {
+		return GroupedOpenApi.builder()
+				         .group("Algafood  API")
+				         .pathsToMatch("/v1/**")
+				         .addOpenApiCustomiser(openApi -> {
+				        	 openApi.info(new Info()
+										.title("Algafood API")
+										.version("v1")
+										.description("REST API do Algafood")
+										.license(new License()
+												.name("Apache 2.0")
+												.url("http://springdoc.com")
+										)
+								 ).externalDocs(new ExternalDocumentation()
+										 .description("AlgaWorks")
+										 .url("https://algaworks.com")
+								 );
+				         }).build();
+	}
+	
+	@Bean
+	GroupedOpenApi groupedOpenApi2() {
+		return GroupedOpenApi.builder()
+				         .group("Algafood  API v2")
+				         .pathsToMatch("/v2/**")
+				         .addOpenApiCustomiser(openApi -> {
+				        	 openApi.info(new Info()
+										.title("Algafood API")
+										.version("v2")
+										.description("REST API do Algafood")
+										.license(new License()
+												.name("Apache 2.0")
+												.url("http://springdoc.com")
+										)
+								 ).externalDocs(new ExternalDocumentation()
+										 .description("AlgaWorks")
+										 .url("https://algaworks.com")
+								 );
+				         }).build();
 	}
 }
