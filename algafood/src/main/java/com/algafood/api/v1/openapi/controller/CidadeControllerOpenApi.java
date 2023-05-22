c49e9c6e7892ac4ffa0edb78d538273132a249f6
Fabricio Macedo <fabriciusiqueira@gmail.com>
2023-05-22 20:46:05 -0300

    Descrevendo códigos de status de respostas em endpoints específicos

--- algafood/src/main/java/com/algafood/api/v1/openapi/controller/CidadeControllerOpenApi.java
+++ algafood/src/main/java/com/algafood/api/v1/openapi/controller/CidadeControllerOpenApi.java
@@ -7,7 +7,10 @@ import com.algafood.api.v1.model.input.CidadeInputDTO;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
@@ -18,7 +21,11 @@ public interface CidadeControllerOpenApi {
 	@Operation(summary = "Lista as cidades")
 	CollectionModel<CidadeDTO> listar();
 
-	@Operation(summary = "Busca uma cidade por id")
+	@Operation(summary = "Busca uma cidade por id", responses = {
+			@ApiResponse(responseCode = "200"),
+			@ApiResponse(responseCode = "400", description = "ID da cidade inválido", 
+					content = @Content(schema = @Schema))
+	})
 	CidadeDTO buscar(@Parameter(description = "ID de uma cidade", example = "1", required = true) Long cidadeId);
 
 	@Operation(summary = "Cadastra uma cidade", description = "Cadastro de uma cidade, necessita de um estado e um nome válido")
