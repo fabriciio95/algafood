213f3d2933c33aaec62d0e55678a941da492152e
Fabricio Macedo <fabriciusiqueira@gmail.com>
2023-05-22 20:02:04 -0300

    Descrevendo modelos de representações e suas propriedades

--- algafood/src/main/java/com/algafood/api/v1/model/CidadeDTO.java
+++ algafood/src/main/java/com/algafood/api/v1/model/CidadeDTO.java
@@ -3,6 +3,7 @@ package com.algafood.api.v1.model;
 import org.springframework.hateoas.RepresentationModel;
 import org.springframework.hateoas.server.core.Relation;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,8 +12,10 @@ import lombok.Setter;
 @Setter
 public class CidadeDTO extends RepresentationModel<CidadeDTO>{
 
+	@Schema(example = "1")
 	private Long id;
 	
+	@Schema(example = "Uberlândia")
 	private String nome;
 	
 	private EstadoDTO estado;
