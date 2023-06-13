79550b31c80aa8a29412d08bc57faab5cd7367d9
Fabricio Macedo <fabriciusiqueira@gmail.com>
2023-06-13 19:45:35 -0300

    Desafio: descrevendo documentação de endpoints de estados

--- algafood/src/main/java/com/algafood/api/v1/openapi/controller/EstadoControllerOpenApi.java
+++ algafood/src/main/java/com/algafood/api/v1/openapi/controller/EstadoControllerOpenApi.java
@@ -4,20 +4,57 @@ import org.springframework.hateoas.CollectionModel;
 
 import com.algafood.api.v1.model.EstadoDTO;
 import com.algafood.api.v1.model.input.EstadoInputDTO;
+import com.algafood.api.v1.openapi.model.EstadosModelOpenApi;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @SecurityRequirement(name = "security_auth")
+@Tag(name = "Estados")
 public interface EstadoControllerOpenApi {
 
+	@Operation(summary = "Lista os estados", responses = {
+			@ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = EstadosModelOpenApi.class)))
+	})
 	CollectionModel<EstadoDTO> listar();
 
-	EstadoDTO buscar(Long estadoId);
+	@Operation(summary = "Busca um estado por ID", responses =  {
+			@ApiResponse(responseCode = "200"),
+			@ApiResponse(responseCode = "400", description = "ID do estado inválido", 
+			             content = @Content(schema = @Schema(ref = "Problema"))),
+			@ApiResponse(responseCode = "404", description = "Estado não encontrado", 
+			             content = @Content(schema = @Schema(ref = "Problema")))
+	})
+	EstadoDTO buscar(@Parameter(example = "1", description = "ID de um estado", required = true) Long estadoId);
 
-	EstadoDTO adicionar(EstadoInputDTO estadoInputDTO);
+	@Operation(summary = "Cadastra um estado", responses =  {
+			@ApiResponse(responseCode = "201", description = "Estado cadastrado")
+	})
+	EstadoDTO adicionar(@RequestBody(description = "Representação de um novo estado", required = true) EstadoInputDTO estadoInputDTO);
 
-	EstadoDTO atualizar(EstadoInputDTO estadoInputDTO, Long estadoId);
+	@Operation(summary = "Atualiza um estado por ID", responses =  {
+			@ApiResponse(responseCode = "200"),
+			@ApiResponse(responseCode = "400", description = "ID do estado inválido", 
+			             content = @Content(schema = @Schema(ref = "Problema"))),
+			@ApiResponse(responseCode = "404", description = "Estado não encontrado", 
+			             content = @Content(schema = @Schema(ref = "Problema")))
+	})
+	EstadoDTO atualizar(@RequestBody(description = "Representação de um estado com os novos dados", required = true)EstadoInputDTO estadoInputDTO,
+			@Parameter(example = "1", description = "ID de um estado", required = true) Long estadoId);
 
-	void excluir(Long estadoId);
+	@Operation(summary = "Exclui um estado por ID", responses =  {
+			@ApiResponse(responseCode = "204"),
+			@ApiResponse(responseCode = "400", description = "ID do estado inválido", 
+			             content = @Content(schema = @Schema(ref = "Problema"))),
+			@ApiResponse(responseCode = "404", description = "Estado não encontrado", 
+			             content = @Content(schema = @Schema(ref = "Problema")))
+	})
+	void excluir(@Parameter(example = "1", description = "ID de um estado", required = true) Long estadoId);
 
 }
--- algafood/src/main/java/com/algafood/api/v1/openapi/model/EstadosModelOpenApi.java
+++ algafood/src/main/java/com/algafood/api/v1/openapi/model/EstadosModelOpenApi.java
@@ -2,24 +2,27 @@ package com.algafood.api.v1.openapi.model;
 
 import java.util.List;
 
-import org.springframework.hateoas.Links;
-
 import com.algafood.api.v1.model.EstadoDTO;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Schema(name = "EstadosDTO")
 public class EstadosModelOpenApi {
 	
+	@Schema(name = "_embedded")
 	private EstadosEmbeddedOpenApi _embedded;
 	
-	private Links _links;
+	@Schema(name = "_links")
+	private LinksModelOpenApi _links;
 
 	
 	@Getter
 	@Setter
+	@Schema(name = "EstadosEmbeddedDTo")
 	public class EstadosEmbeddedOpenApi {
 		
 		List<EstadoDTO> estados;
