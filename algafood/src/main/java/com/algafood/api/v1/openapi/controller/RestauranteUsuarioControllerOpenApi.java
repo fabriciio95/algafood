1f7cce5c058db49de66979adf3f05010bbc31470
Fabricio Macedo <fabriciusiqueira@gmail.com>
2023-06-13 20:23:48 -0300

    Desafio: descrevendo documentação de endpoints de associação de restaurantes com usuários

--- algafood/src/main/java/com/algafood/api/v1/openapi/controller/RestauranteUsuarioControllerOpenApi.java
+++ algafood/src/main/java/com/algafood/api/v1/openapi/controller/RestauranteUsuarioControllerOpenApi.java
@@ -4,17 +4,49 @@ import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.ResponseEntity;
 
 import com.algafood.api.v1.model.UsuarioDTO;
+import com.algafood.api.v1.openapi.model.UsuariosModelOpenApi;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @SecurityRequirement(name = "security_auth")
+@Tag(name = "Restaurantes")
 public interface RestauranteUsuarioControllerOpenApi {
 
-	
-	CollectionModel<UsuarioDTO> listar(Long restauranteId);
+	@Operation(summary = "Lista os usuários responsáveis associadas a um restaurante", responses = {
+			@ApiResponse(responseCode = "200", 
+					content = @Content(schema = @Schema(implementation = UsuariosModelOpenApi.class))),
+			@ApiResponse(responseCode = "404", description = "Restaurante não encontrado", 
+            		content = @Content(schema = @Schema(ref = "Problema")))
+	})
+	CollectionModel<UsuarioDTO> listar(@Parameter(example = "1", description = "ID de um restaurante", 
+            required = true) Long restauranteId);
 
-	ResponseEntity<Void> associarResponsavel(Long restauranteId, Long usuarioId);
+	@Operation(summary = "Associação de restaurante com usuário responsável", responses = {
+			@ApiResponse(responseCode = "204", description = "Usuário responsável associado"),
+			@ApiResponse(responseCode = "400", description = "Requisição inválida", 
+			             content = @Content(schema = @Schema(ref = "Problema"))),
+			@ApiResponse(responseCode = "404", description = "Restaurante ou usuário não encontrado", 
+            content = @Content(schema = @Schema(ref = "Problema")))
+	})
+	ResponseEntity<Void> associarResponsavel(@Parameter(example = "1", description = "ID de um restaurante", 
+            required = true) Long restauranteId, @Parameter(example = "1",
+            description = "ID de um usuário", required = true) Long usuarioId);
 
-	ResponseEntity<Void> deassociarResponsavel(Long restauranteId, Long usuarioId);
+	@Operation(summary = "Desassociação de restaurante com usuário responsável", responses = {
+			@ApiResponse(responseCode = "204", description = "Usuário responsável desassociado"),
+			@ApiResponse(responseCode = "400", description = "Requisição inválida", 
+			             content = @Content(schema = @Schema(ref = "Problema"))),
+			@ApiResponse(responseCode = "404", description = "Restaurante ou usuário não encontrado", 
+            content = @Content(schema = @Schema(ref = "Problema")))
+	})
+	ResponseEntity<Void> deassociarResponsavel(@Parameter(example = "1", description = "ID de um restaurante", 
+            required = true) Long restauranteId, @Parameter(example = "1",
+            description = "ID de um usuário", required = true) Long usuarioId);
 
 }
--- algafood/src/main/java/com/algafood/api/v1/openapi/model/UsuariosModelOpenApi.java
+++ algafood/src/main/java/com/algafood/api/v1/openapi/model/UsuariosModelOpenApi.java
@@ -2,21 +2,24 @@ package com.algafood.api.v1.openapi.model;
 
 import java.util.List;
 
-import org.springframework.hateoas.Links;
-
 import com.algafood.api.v1.model.UsuarioDTO;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
+@Schema(name = "UsuariosModelDTO")
 public class UsuariosModelOpenApi {
 	
+	@Schema(name = "_embedded")
 	private UsuariosEmbeddedModelOpenApi _embedded;
 	
-	private Links _links;
+	@Schema(name = "_links")
+	private LinksModelOpenApi _links;
 
 	
 	@Data
+	@Schema(name = "UsuariosEmbeddedDTO")
 	public class UsuariosEmbeddedModelOpenApi {
 		
 		List<UsuarioDTO> usuarios;
