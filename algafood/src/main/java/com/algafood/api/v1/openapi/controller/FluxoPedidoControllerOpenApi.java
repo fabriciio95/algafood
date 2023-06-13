60ffcc78af7503dfce406acc00525789d0c03639
Fabricio Macedo <fabriciusiqueira@gmail.com>
2023-06-13 19:55:53 -0300

    Desafio: descrevendo documentação de endpoints de fluxo de pedidos

--- algafood/src/main/java/com/algafood/api/v1/openapi/controller/FluxoPedidoControllerOpenApi.java
+++ algafood/src/main/java/com/algafood/api/v1/openapi/controller/FluxoPedidoControllerOpenApi.java
@@ -2,15 +2,40 @@ package com.algafood.api.v1.openapi.controller;
 
 import org.springframework.http.ResponseEntity;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @SecurityRequirement(name = "security_auth")
+@Tag(name = "Pedidos")
 public interface FluxoPedidoControllerOpenApi {
 
-	ResponseEntity<Void> confirmar(String codigoPedido);
+	@Operation(summary = "Confirmação de pedido", responses = {
+			@ApiResponse(responseCode = "204", description = "Pedido confirmado"),
+			@ApiResponse(responseCode = "404", description = "Pedido não encontrado", 
+			             content = @Content(schema = @Schema(ref = "Problema")))
+	})
+	ResponseEntity<Void> confirmar(@Parameter(example = "8f2d22a7-9f3b-421e-9377-4c3b77b34924", 
+			description = "Código do pedido", required = true) String codigoPedido);
 
-	ResponseEntity<Void> cancelar(String codigoPedido);
+	@Operation(summary = "Cancelamento de pedido", responses = {
+			@ApiResponse(responseCode = "204", description = "Pedido cancelado"),
+			@ApiResponse(responseCode = "404", description = "Pedido não encontrado", 
+			             content = @Content(schema = @Schema(ref = "Problema")))
+	})
+	ResponseEntity<Void> cancelar(@Parameter(example = "8f2d22a7-9f3b-421e-9377-4c3b77b34924", 
+			description = "Código do pedido", required = true) String codigoPedido);
 	
-	ResponseEntity<Void> entregar(String codigoPedido);
+	@Operation(summary = "Registrar entrega de pedido", responses = {
+			@ApiResponse(responseCode = "204", description = "Pedido entregue"),
+			@ApiResponse(responseCode = "404", description = "Pedido não encontrado", 
+			             content = @Content(schema = @Schema(ref = "Problema")))
+	})
+	ResponseEntity<Void> entregar(@Parameter(example = "8f2d22a7-9f3b-421e-9377-4c3b77b34924", 
+			description = "Código do pedido", required = true) String codigoPedido);
 
 }
