cdf69e86e4561c64454c5c83cb27a7fa6bf64a32
Fabricio Macedo <fabriciusiqueira@gmail.com>
2023-06-13 21:16:01 -0300

    Desafio: descrevendo documentação de endpoints de fotos de produtos

--- algafood/src/main/java/com/algafood/api/v1/model/FotoProdutoDTO.java
+++ algafood/src/main/java/com/algafood/api/v1/model/FotoProdutoDTO.java
@@ -3,12 +3,16 @@ package com.algafood.api.v1.model;
 import org.springframework.hateoas.RepresentationModel;
 import org.springframework.hateoas.server.core.Relation;
 
+import com.algafood.api.v1.openapi.model.FotoProdutoOpenApiDTO;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
 @Relation(collectionRelation = "fotos")
 @Getter
 @Setter
+@Schema(implementation = FotoProdutoOpenApiDTO.class)
 public class FotoProdutoDTO extends RepresentationModel<FotoProdutoDTO> {
 	
 	private String nomeArquivo;
--- algafood/src/main/java/com/algafood/api/v1/openapi/controller/RestauranteProdutoFotoControllerOpenApi.java
+++ algafood/src/main/java/com/algafood/api/v1/openapi/controller/RestauranteProdutoFotoControllerOpenApi.java
@@ -15,8 +15,10 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @SecurityRequirement(name = "security_auth")
+@Tag(name = "Produtos")
 public interface RestauranteProdutoFotoControllerOpenApi {
 
 	@Operation(summary = "Busca foto do produto de um restaurante", responses = {
@@ -24,21 +26,42 @@ public interface RestauranteProdutoFotoControllerOpenApi {
 					@Content(mediaType = "application/json", schema = @Schema(implementation = FotoProdutoDTO.class)),
 					@Content(mediaType = "image/jpeg", schema = @Schema(type = "string", format = "binary")),
 					@Content(mediaType = "image/png", schema = @Schema(type = "string", format = "binary"))
-			})
+			}),
+			@ApiResponse(responseCode = "404", description = "Restaurante ou produto não encontrado", 
+			        content = @Content(schema = @Schema(ref = "Problema"))),
+			@ApiResponse(responseCode = "400", description = "Requisição inválida", 
+	        content = @Content(schema = @Schema(ref = "Problema")))
 	})
-	FotoProdutoDTO buscar(Long restauranteId,  Long produtoId);
+	FotoProdutoDTO buscar(@Parameter(example = "1", description = "ID de um restaurante", 
+		    required = true)Long restauranteId, 
+			@Parameter(example = "1", description = "ID de um produto", required = true) Long produtoId);
 
 	@Operation(hidden = true)
 	ResponseEntity<?> servirFoto(Long restauranteId, Long produtoId,  String acceptHeader)
 			throws HttpMediaTypeNotAcceptableException;
 
-	@Operation(summary = "Atualiza a foto do produto de um restaurante")
+	@Operation(summary = "Atualiza a foto do produto de um restaurante", responses = {
+			@ApiResponse(responseCode = "200"),
+			@ApiResponse(responseCode = "400", description = "Requisição inválida",
+			             content = @Content(schema = @Schema(ref = "Problema"))),
+			@ApiResponse(responseCode = "404", description = "Restaurante ou produto não encontrado", 
+			             content = @Content(schema = @Schema(ref = "Problema")))
+	})
 	FotoProdutoDTO atualizarFoto(@Parameter(description = "ID do restaurante", example = "1", required = true) Long restauranteId, 
 								 @Parameter(description = "ID do produto", example = "1", required = true)Long produtoId, 
 			                     @RequestBody(required = true) FotoProdutoInputDTO fotoProdutoInput)
 			throws IOException;
 
 	
-	void excluir(Long restauranteId, Long produtoId);
+	@Operation(summary = "Exclui uma foto de um produto de um restaurante por ID", responses =  {
+			@ApiResponse(responseCode = "204", description = "Foto excluída"),
+			@ApiResponse(responseCode = "400", description = "Requisição inválida",
+			             content = @Content(schema = @Schema(ref = "Problema"))),
+			@ApiResponse(responseCode = "404", description = "Restaurante ou produto não encontrado", 
+			             content = @Content(schema = @Schema(ref = "Problema")))
+	})
+	void excluir(@Parameter(example = "1", description = "ID de um restaurante", 
+		    required = true) Long restauranteId, 
+			@Parameter(example = "1", description = "ID de um produto", required = true) Long produtoId);
 
 }
