5e16a4332e1bf5bf9ee6bcaaa73a5d50ca78cb8b
Fabricio Macedo <fabriciusiqueira@gmail.com>
2023-06-20 20:11:41 -0300

    Armazenando autorizações no banco de dados

--- algafood/src/main/java/com/algafood/core/security/authorizationserver/AuthorizationServerConfig.java
+++ algafood/src/main/java/com/algafood/core/security/authorizationserver/AuthorizationServerConfig.java
@@ -7,17 +7,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.OAuth2TokenFormat;
+import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationService;
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.ProviderSettings;
 import org.springframework.security.oauth2.server.authorization.config.TokenSettings;
+import org.springframework.security.oauth2.server.authorization.web.OAuth2AuthorizationServerMetadataEndpointFilter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -55,5 +59,12 @@ public class AuthorizationServerConfig {
 				 
 		
 		return new InMemoryRegisteredClientRepository(Arrays.asList(algafoodBackend));
+	}
+	
+	@Bean
+	OAuth2AuthorizationService oAuth2AuthorizationService(JdbcOperations jdbcOperations,
+			RegisteredClientRepository registeredClientRepository) {
+		
+		return new JdbcOAuth2AuthorizationService(jdbcOperations, registeredClientRepository);
 	}
 }
